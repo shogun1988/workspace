e1ff465ef5755e6f0ce2bdb731d0afd2ce44d77a
CodeSandbox <support@codesandbox.io>
2023-10-21 13:45:20 +0000

    Sandbox snapshot commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+    <div>
+        <h1>Hello World</h1><p>This is a paragraph</p>
+    </div>,
+    document.getElementById("root")
+);
